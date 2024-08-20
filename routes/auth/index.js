6c27ce60e67036771e79f5083b4b39dbaac08e53
francis-tg <franc9885@gmail.com>
2024-08-20 06:36:32 +0000

    fix some errors

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -5,7 +5,7 @@ const router = require("express").Router()
 
 router.get("/login",auth.loginController)
 router.post("/login",ValidateField, auth.authController)
-router.get("/register",auth.registerController)
-router.post("/register",auth.createAccountController)
+/* router.get("/register",auth.registerController)
+router.post("/register",auth.createAccountController) */
 router.get("/logout",auth.logoutController)
 module.exports = router
