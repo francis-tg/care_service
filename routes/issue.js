2672b63da04f755745dbf6feb2445929ab2bed57
francis-tg <franc9885@gmail.com>
2024-08-24 01:48:50 +0000

    fix remove issueé

--- routes/issue.js
+++ routes/issue.js
@@ -8,6 +8,6 @@ const router =require('express').Router()
 router.get('/',ensureAuthenticated, issue.getIssue)
 router.post('/',ensureAuthenticated, issue.createIssue)
 router.post('/update/:id',ensureAuthenticated,ValidateField,issue.updateIssue)
-router.post("/remove/:id",ensureAuthenticated,issue.deleteIssue)
+router.get("/remove/:id",ensureAuthenticated,issue.deleteIssue)
 
 module.exports = router
